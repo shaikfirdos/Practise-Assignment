eb456e79921329501ed480a3fd53810e6d693745
shaikfirdos <72145860+shaikfirdos@users.noreply.github.com>
2020-11-16 09:28:23 -0800

    Add files via upload

---
+++ MvcAppliaction/MyApp.Model/Address.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.Model
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Details { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
